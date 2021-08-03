ec0d3bf47858e98836ce55606aa6167174d12c94
Dima <damios.1812@gmail.com>
2021-08-03 18:02:48 +0300

    цвета, шрифты перенес в отдельный файл vars, изменил значения line-height, теперь стоят в числах, перенес скрипты в отдельный файл, исправил slick-dots - теперь та не перерождает другим элементам, убрал фикс. высоту у блоков, убрал отпечатки в классах, а также некоторые переименовал, поставил везде в стилях & амперсанты, подправил шрифты, все media файлы рассортировал в отдельные файлы,  убрал дубляж кода, переименовал main, переменные,  убрал дефолтные стили

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+$('.display__slider').slick({
+    infinite: true,
+    dots: true,
+    dotsClass: 'slick-dots',
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+let list = anime({
+    targets: '.header__li',
+    translateX: -200,                
+    delay: anime.stagger(50, {
+        start: 1000,
+    }),
+    easing: 'easeInOutQuad'           
+});    
+
+let seo = anime({
+    targets: '.header__h4',
+    translateX: -200,                
+    delay: 1000,
+    easing: 'easeInOutQuad'           
+}); 
+let about = anime({
+    targets: '.header__p',
+    translateX: -200,                
+    delay: 1000,
+    easing: 'easeInOutQuad'           
+});      
+let number = anime({
+    targets: '.header__h6',
+    translateX: -60,                
+    delay: 1150,
+    easing: 'easeInOutQuad'           
+});   
+let sity = anime({
+    targets: '.header__h5',
+    translateX: -120,                
+    delay: 1100,
+    easing: 'easeInOutQuad'           
+}); 
+let year = anime({
+    targets: '.header__year',
+    translateX: -80,                
+    delay: 1000,
+    easing: 'easeInOutQuad'           
+});     
+let bitTitle = anime({
+    targets: '.header__h2, .header__h3',
+    translateY: -200,                
+    delay: 220,
+    easing: 'easeInOutQuad'           
+});   
+$('.header__slider').slick({
+    infinite: true,
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+    touchMove: true,
+    touchThreshold: 30,
+    speed: 500
+}); 
