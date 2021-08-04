49107cb2e9d7420452d51cbfe9d4952a76620d51
Dima <damios.1812@gmail.com>
2021-08-04 14:35:25 +0300

    добавил поинт для слайдеров

--- js/scripts.js
+++ js/scripts.js
@@ -10,49 +10,48 @@ $('.display__slider').slick({
 
 let list = anime({
     targets: '.header__li',
-    translateX: -200,                
+    translateX: [200, 0],                
     delay: anime.stagger(50, {
         start: 1000,
     }),
     easing: 'easeInOutQuad'           
 });    
-
 let seo = anime({
     targets: '.header__h4',
-    translateX: -200,                
+    translateX: [200, 0],                
     delay: 1000,
     easing: 'easeInOutQuad'           
 }); 
 let about = anime({
     targets: '.header__p',
-    translateX: -200,                
+    translateX: [200, 0],                
     delay: 1000,
     easing: 'easeInOutQuad'           
 });      
 let number = anime({
     targets: '.header__h6',
-    translateX: -60,                
+    translateX: [70, 0],                
     delay: 1150,
     easing: 'easeInOutQuad'           
 });   
 let sity = anime({
     targets: '.header__h5',
-    translateX: -120,                
+    translateX: [120, 0],                
     delay: 1100,
     easing: 'easeInOutQuad'           
 }); 
 let year = anime({
     targets: '.header__year',
-    translateX: -80,                
+    translateX: [80, 0],                
     delay: 1000,
     easing: 'easeInOutQuad'           
 });     
 let bitTitle = anime({
     targets: '.header__h2, .header__h3',
-    translateY: -200,                
+    translateY: [200, 0],                
     delay: 220,
     easing: 'easeInOutQuad'           
-});   
+});  
 $('.header__slider').slick({
     infinite: true,
     dots: false,
